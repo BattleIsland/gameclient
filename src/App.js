2f060b37ce49d12e23dd2ca2c7cd25915d68d2d8
Roddy Taipe <rodtai2019@gmail.com>
2023-11-12 17:38:19 -0800

    fix: Able to connect to websocket endpoint

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import {Client} from '@stomp/stompjs';
+import {useEffect, useRef} from 'react';
+
 
 function App() {
+  const client = useRef();
+
+  useEffect(() => {
+    const newClient = new Client({
+      brokerURL: 'ws://localhost:8080/websocket-endpoint',
+      onConnect: () => {
+        newClient.subscribe('/topic/messages', message =>
+          console.log(`Received: ${message.body}`)
+        );
+        // client.publish({ destination: '/topic/messages', body: 'First Message' });
+      },
+    })
+    newClient.activate();
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
